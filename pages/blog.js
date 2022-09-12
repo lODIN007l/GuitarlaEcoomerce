0a1c8450f4cc3c4c231a997944024a51806420c2
Wilson Lluilema <wilsonlluilema007@gmail.com>
2022-09-12 00:52:10 -0500

    consulta de api blog with strapi

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,17 @@
-import Link from "next/link";
+import { useEffect } from "react";
 import Layouts from "../components/Layouts";
 const Blog = () => {
+  //consulta Api
+  useEffect(() => {
+    const consultarApi = async () => {
+      const url = "http://localhost:1337/blogs";
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+      console.log(resultado);
+    };
+    consultarApi();
+  }, []);
+
   return (
     <Layouts pagina="blog">
       <h1>Desde Blog</h1>
