b364d8b7eb1ef999a8a48d3c573dbe4eab661474
Wilson Lluilema <wilsonlluilema007@gmail.com>
2022-09-14 04:30:01 -0500

    tienda end

---
+++ pages/guitarras/[url].js
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import Layouts from "../../components/Layouts";
+import styles from "../../styles/Guitarra.module.css";
+
+const Producto = ({ guitarra }) => {
+  const { descripcion, imagen, nombre, url, precio } = guitarra[0];
+  return (
+    <Layouts pagina={`Guitarra ${nombre}`}>
+      <div className={styles.guitarra}>
+        <Image
+          layout="responsive"
+          src={imagen.url}
+          width={180}
+          height={350}
+          alt="imagen guitarra"
+        />
+        <div className={styles.contenido}>
+          <h3>{nombre}</h3>
+          <p className={styles.descripcion}>{descripcion}</p>
+          <p className={styles.precio}>{`$ ${precio}`}</p>
+          <form className={styles.formulario}>
+            <label>Cantidad :</label>
+            <select>
+              <option value="">-- Seleccion --</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+            <input type="submit" value="Agregar al carrito" />
+          </form>
+        </div>
+      </div>
+    </Layouts>
+  );
+};
+
+export default Producto;
+
+export async function getServerSideProps({ query: { url } }) {
+  // console.log(url);
+  const urlGuitarra = `${process.env.API_URL}/guitarras?url=${url}`;
+  const respuesta = await fetch(urlGuitarra);
+  const guitarra = await respuesta.json();
+  // console.log(guitarra);
+  return {
+    props: {
+      guitarra,
+    },
+  };
+}
